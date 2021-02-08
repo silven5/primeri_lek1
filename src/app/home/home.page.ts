3d6911c1c6374eef565fa83cba672774dbac77e7
silven5 <elvenff@gmail.com>
2021-02-08 19:00:42 +0200

    master

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  rezult: string[] ;
   constructor() {}
-
+  ras(nn: string) {
+    let n;
+    this.rezult= new Array();
+    n = parseInt(nn);
+    for (let i = 1; i <= n; i++) {
+      let str = "";
+      if (i % 3 == 0) str = "Fizz";
+      if (i % 5 == 0) str += "Buzz";
+      if (str == "") str = i.toFixed(0);
+      this.rezult.push(str);
+    }
+  }
 }
